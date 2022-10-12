d705ca9d7879713d3fc0adb3af36866e4cb3fdff
KASTHANEDA <kasthaneda@gmail.com>
2022-10-11 22:09:36 -0500

    prueba

---
+++ ARROW-FUNTIONS/src/main.js
@@ -0,0 +1,7 @@
+const estudiantes  = ['Juian','Bghsghg','Cghgh','Ddfsfg','ghgE'];
+
+console.log(estudiantes.map(estudiantes=>estudiantes.length));
+
+estudiantes.forEach(x=> console.log(estudiantes));
+estudiantes.forEach(x=> console.log(x));
+console.log(estudiantes.length)

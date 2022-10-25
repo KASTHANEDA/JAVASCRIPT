b295c27bbf5595ac88ef464f3c49de51c5370c08
KASTHANEDA <kasthaneda@gmail.com>
2022-10-25 18:17:58 -0500

    ok

--- CAJERO/SRC/consulta.js
+++ CAJERO/SRC/consulta.js
@@ -1,34 +1,30 @@
-let saldo = ['95222,52','','']
-console.log(saldo)
 
 
-
-
-saldo.forEach(function(saldo) {
-    console.log(saldo);    
-})
-
-document.getElementById("saldo").innerHTML = (saldo);    
+   let saldo =parseInt(654689877);
+    let saldo2;
 
 function redirect() {
     location.replace("opcCajero.html");
-}
+};
 
 function retirar() {
-    let saldo2 = document.getElementById("retirar").value;
-    if (saldo2=''){
-        saldo.forEach(function(saldo) {
-            
-            console.log(saldo2);    
-            saldo=saldo-saldo2;
-            
-        }
-        console.log(saldo);    
-        )
+     saldo2 =parseInt(document.getElementById("retirar").value);
+
+    console.log('Saldo a retirar de '+saldo2);
+    if (saldo2>0){
+        document.getElementById("saldo").innerHTML = (saldo);  
+            console.log('Saldo a retirar de '+saldo2);
+            saldo2=saldo-saldo2;    
+            console.log(saldo2);     
+          
     }
+      
     else {
+       
         console.log('Entre aqui ')
-    }
+    };
+    return (document.getElementById("saldo").innerHTML = (saldo2));  
+    
     
  
-}
+};
--- CAJERO/SRC/opcCajero.js
+++
@@ -1,5 +0,0 @@
-
-let vegetales = ['Repollo', 'Nabo', 'Rábano', 'Zanahoria'];
-console.log(vegetales);
-
-document.getElementById("Cajero").innerHTML = vegetales[1];
